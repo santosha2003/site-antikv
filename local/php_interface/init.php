09f7d33bdc4c857d805970ea47d440bed20ec74c
santosha2003 <36627236+santosha2003@users.noreply.github.com>
2021-11-25 16:06:57 +0300

    php74 FreeBSD

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,20 +1,42 @@
 <?php
+function debugLog($ar, $file_name = 'debug.txt'){
+	$info = debug_backtrace();
+	$file=$info[0]['file'];
+	$line=$info[0]['line'];
+	if(isset($info[1]['function'])):
+		//Отображение функции с которой был вызов
+		$function = "FUNC: ".$info[1]['function']."()";
+	endif;
+
+	if (!empty($file) && !empty($line)):
+		$f=fopen($_SERVER['DOCUMENT_ROOT']."/debug/".$file_name,"a");
+		//if($utf_8!==false) fwrite($f,'<meta http-equiv="Content-Type" content="text/html; charset='.$utf_8.'" />\n');
+		fwrite($f,date("d.m.Y H:i:s")."\n");
+		fwrite($f,"FILE: ".$file."\n");
+		fwrite($f,"LINE: ".$line."\n");
+		fwrite($f,$function."\n");
+		if(is_bool($ar) === true):
+			fwrite($f,"bool(".var_export($ar,true).")\n\n");
+		else:
+			fwrite($f,print_r($ar,true)."\n\n");
+		endif;
+		fclose($f);
+	else:
+		die("debug: не указан файл или строка");
+	endif;
+}
+
+
 //Подключаем файл с функциями
-<<<<<<< HEAD
-require_once ($_SERVER["DOCUMENT_ROOT"]. "/bitrix/php_interface/watermark1.php");
-=======
 require_once ($_SERVER["DOCUMENT_ROOT"]. "/local/php_interface/watermark1.php");
->>>>>>> 4bb3e4deb359749a96a02a5e4d7c22ab1399e137
+
 //Создание элемента
 AddEventHandler("iblock", "OnBeforeIBlockElementAdd",array("CWatermark", "ImageAdd"));
 //Изменение элемента
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate",array("CWatermark", "ImageUpdate"));
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 4bb3e4deb359749a96a02a5e4d7c22ab1399e137
 // обработчик события регистрации
 AddEventHandler("main", "OnBeforeUserRegister", "OnBeforeUserRegisterHandler");
 // обработчик события обновления данных пользователем
@@ -27,7 +49,6 @@ AddEventHandler("main", "OnBeforeUserUpdate", "OnBeforeUserRegisterHandler");
     }
 
 function custom_mail($to,$subject,$body,$headers) {
-<<<<<<< HEAD
 $f4=fopen($_SERVER["DOCUMENT_ROOT"]."/log-mail.txt","a+");
 //    fwrite($f3, print_r($imgprop,true));//печатаем в файл результирующий массив для проверки
 fwrite($f4, print_r(array(‘TO’ => $to, ‘SUBJECT’ => $subject, ‘BODY’ => $body, ‘HEADERS’ => $headers),1)."n========n");
@@ -36,26 +57,13 @@ return mail($to,$subject,$body,$headers);
 }
 
 
-=======
-$f4a=fopen($_SERVER["DOCUMENT_ROOT"]."/log-mail1.txt","a+");
-//    fwrite($f3, print_r($imgprop,true));//печатаем в файл результирующий массив для проверки
-fwrite($f4a, print_r(array(‘TO’ => $to, ‘SUBJECT’ => $subject, ‘BODY’ => $body, ‘HEADERS’ => $headers),1)."n========n");
-fclose($f4a);
-return mail($to,$subject,$body,$headers);
-}
-
->>>>>>> 4bb3e4deb359749a96a02a5e4d7c22ab1399e137
 function OnBeforeUserRegisterHandler($args) {
 	if(isset($_POST['c']) && $_POST['c'] != $_POST['s']) {
 		$GLOBALS['APPLICATION']->ThrowException('Произошла внутренняя ошибка, попробуйте еще раз');
 		return false;
 	}
 
-<<<<<<< HEAD
    	if (!preg_match("/^[а-яА-Я]+$/ui", $args['NAME'])) {
-=======
-   	if (!preg_match("/^[а-яА-Я]+$/ui", $args['NAME'])) {               // if utf-8 use /ui modifier else /i
->>>>>>> 4bb3e4deb359749a96a02a5e4d7c22ab1399e137
    		if(empty($args['NAME'])) {
 	   		$GLOBALS['APPLICATION']->ThrowException('Имя обязательное поле');
 	   		return false;
@@ -79,4 +87,11 @@ function OnBeforeUserRegisterHandler($args) {
 
     return true;
 }
+
+
+
+// Catalog: Generate transparent watermark on iblock update
+//require_once 'watermark-rusanti.php';
+require_once dirname(__FILE__) . '/watermark-rusanti.php';
+
 ?>
